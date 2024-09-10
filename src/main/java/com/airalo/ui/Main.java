289b93553108ae391fb53a315ccbda9c7857e8a7
Piyush231090 <x22154779@student.ncirl.ie>
2024-09-10 01:05:35 +0100

    approach 2

--- src/main/java/com/airalo/ui/Main.java
+++ src/main/java/com/airalo/ui/Main.java
@@ -3,6 +3,9 @@ package com.airalo.ui;
 import com.airalo.commons.URL;
 import com.airalo.util.Driver;
 import com.airalo.util.UIComponents;
+
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -25,10 +28,18 @@ public class Main {
             uiComponents.buyNowButton.click();
 
             assert "Moshi Moshi".equals(uiComponents.title.getText());
+            //Approach 1, taking all the details in a list of webelements
+            List<WebElement> details = driver.findElements(By.xpath("//ul[@class='sim-detail-info-list']"));
+            assert "Japan".equals(details.get(1).getText());
+            assert "1 GB".equals(details.get(3).getText());
+            assert "7 days".equals(details.get(5).getText());
+            assert "4.50".startsWith(details.get(7).getText());
+
+            // Approach2, considering all webelements individually and comparing 
             assert "Japan".equals(uiComponents.coverage.getText());
             assert "1 GB".equals(uiComponents.data.getText());
             assert "7 days".equals(uiComponents.validity.getText());
-            assert "$4.50".equals(uiComponents.price.getText());
+            assert "4.50".startsWith(uiComponents.price.getText());
         } catch (Exception exception) {
             assert false;
         } finally {
